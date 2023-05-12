8aec375ea2726c4704ec3d9cee5c3daad86569a9
ilias <hliaslago@gmail.com>
2023-05-12 20:44:24 +0300

    start building auth system

--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/SpringBackendHmsApplication.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/SpringBackendHmsApplication.java
@@ -3,12 +3,11 @@ package com.hms.springbackendhms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Health Managment System Server
+// Health Management System Server
 // Spring Boot Application
 
 @SpringBootApplication
 public class SpringBackendHmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendHmsApplication.class, args);
 	}
---
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.hms.springbackendhms.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+                                throws ServletException, IOException {
+
+    }
+}
