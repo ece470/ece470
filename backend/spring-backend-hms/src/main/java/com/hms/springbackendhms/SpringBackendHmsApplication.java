aa34ff9b40d1c1b839d65083b2bf93bc147b0e86
Ilias Lagomatis <hliaslago@gmail.com>
2023-05-12 12:30:26 +0300

    Add files via upload
    backend: setup spring environment

---
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/SpringBackendHmsApplication.java
@@ -0,0 +1,16 @@
+package com.hms.springbackendhms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Health Managment System Server
+// Spring Boot Application
+
+@SpringBootApplication
+public class SpringBackendHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendHmsApplication.class, args);
+	}
+
+}
---
+++ backend/spring-backend-hms/src/test/java/com/hms/springbackendhms/SpringBackendHmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.springbackendhms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendHmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
