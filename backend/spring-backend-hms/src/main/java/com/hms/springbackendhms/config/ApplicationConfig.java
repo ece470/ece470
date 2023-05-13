5a30d63c9326ef2bd0d27c293420ba295a2c598b
ilias <hliaslago@gmail.com>
2023-05-13 14:43:26 +0300

    Add authentication endpoint

--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/config/ApplicationConfig.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/config/ApplicationConfig.java
@@ -4,8 +4,11 @@ import com.hms.springbackendhms.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -28,6 +31,11 @@ public class ApplicationConfig {
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
