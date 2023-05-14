9783cd0b155a9a1aa30df0ee9664b28d4c1fde74
Anastasia <amallikopoulou@uth.gr>
2023-05-14 13:30:12 +0300

    Created AuthorizedController to send login page to the user

--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/config/SecurityConfiguration.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/api/v1/auth/**", "/home/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/controller/AuthorizedController.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/controller/AuthorizedController.java
@@ -1,17 +1,19 @@
-package com.hms.springbackendhms.demo;
+package com.hms.springbackendhms.controller;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
-public class DemoController {
+@RequestMapping("/home")
+@RequiredArgsConstructor
+public class AuthorizedController {
 
     @GetMapping("/login")
-    public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("Hello from secure endpoint");
+    public ResponseEntity<String> login(){
+        return ResponseEntity.ok("Login Page");
     }
-
 
 }
