5ab0587d32b5d3e9806629714e26007920fdf26c
ilias <hliaslago@gmail.com>
2023-05-18 16:31:39 +0300

    Main page controller implementation

--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/controller/MainPageController.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/controller/MainPageController.java
@@ -3,26 +3,47 @@ package com.hms.springbackendhms.controller;
 import com.hms.springbackendhms.config.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class MainPageController {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @GetMapping
-    public ResponseEntity<String> mainPage(
+    public String mainPage(
             @CookieValue(name = "token", defaultValue = "") String token
     )
     {
         if(token.isBlank()){
-            return ResponseEntity.ok("Disconnected");
+            return "register";
+            //return home page
         }
-        String email = jwtService.extractUsername(token);
-        return ResponseEntity.ok("Hello from secure endpoint, user: " + email);
+        String userEmail = jwtService.extractUsername(token);
+        if(userEmail != null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(token, userDetails)) {
+                // access database to get info about the user
+                // and return main page
+                return "Valid token: " + userEmail;
+
+            } else {
+                // return home page
+                return "register";
+            }
+
+        }
+        return "register";
+        // return home page
     }
 }
