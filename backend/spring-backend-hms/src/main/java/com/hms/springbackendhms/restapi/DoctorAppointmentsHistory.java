681aae90b6438bc14a47314f2e62f5c60308843b
Anastasia <amallikopoulou@uth.gr>
2023-05-23 17:54:10 +0300

    started building doctor appointments history service

--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/restapi/DoctorAppointmentsHistory.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/restapi/DoctorAppointmentsHistory.java
@@ -1,18 +1,15 @@
 package com.hms.springbackendhms.restapi;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.hms.springbackendhms.response.HistoryResponse;
-import jakarta.persistence.Entity;
+import com.hms.springbackendhms.response.DoctorAppointmentsHistoryResponse;
+import com.hms.springbackendhms.util.DoctorAppointment;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/restapi/doctor_appointments_history")
@@ -20,11 +17,22 @@ import java.util.Map;
 public class DoctorAppointmentsHistory {
 
     @GetMapping
-    public HistoryResponse history()
-    {
-        return HistoryResponse.builder()
-                .fname("ilias")
-                .lname("lagomatis")
+    public DoctorAppointmentsHistoryResponse history() {
+        ArrayList<DoctorAppointment> appointments = new ArrayList<>();
+        appointments.add(DoctorAppointment.builder()
+                        .patientLastname("Lagomatis")
+                        .patientFirstname("Ilias")
+                        .date(new Date())
+                        .build());
+
+        appointments.add(DoctorAppointment.builder()
+                .patientLastname("Mallikopoulou")
+                .patientFirstname("Anastasia")
+                .date(new Date())
+                .build());
+
+        return DoctorAppointmentsHistoryResponse.builder()
+                .history(appointments)
                 .build();
     }
 }
--- backend/spring-backend-hms/target/classes/static/restapi/doctorAppointmentsHistory.js
+++ backend/spring-backend-hms/target/classes/static/restapi/doctorAppointmentsHistory.js
@@ -3,6 +3,10 @@
 axios.get('/restapi/doctor_appointments_history')
     .then(function (response) {
         console.log("got response: " + JSON.stringify(response.data));
+        const history = response.data.history;
+        history.forEach(element => {
+            console.log("Element: " + JSON.stringify(element))
+        });
         /*if(response.data){
             const parsed = JSON.parse(response.data);
             console.log(parsed);
