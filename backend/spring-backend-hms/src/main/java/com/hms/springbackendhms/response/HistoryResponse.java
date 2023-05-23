19168739b73e48025c4a347c1663582283fae58f
Anastasia <amallikopoulou@uth.gr>
2023-05-23 16:31:56 +0300

    communicating using jsons

---
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/response/HistoryResponse.java
@@ -0,0 +1,15 @@
+package com.hms.springbackendhms.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HistoryResponse {
+    String fname;
+    String lname;
+}
--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/restapi/DoctorAppointmentsHistory.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/restapi/DoctorAppointmentsHistory.java
@@ -1,4 +1,30 @@
 package com.hms.springbackendhms.restapi;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.hms.springbackendhms.response.HistoryResponse;
+import jakarta.persistence.Entity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/restapi/doctor_appointments_history")
+@RequiredArgsConstructor
 public class DoctorAppointmentsHistory {
+
+    @GetMapping
+    public HistoryResponse history()
+    {
+        return HistoryResponse.builder()
+                .fname("ilias")
+                .lname("lagomatis")
+                .build();
+    }
 }
