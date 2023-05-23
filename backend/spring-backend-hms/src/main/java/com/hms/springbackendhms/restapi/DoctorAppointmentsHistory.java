7afaeabd1b0e9c6e424e6b297fe3bdffd306e4ed
Anastasia <amallikopoulou@uth.gr>
2023-05-23 21:10:28 +0300

    Doctor Appointments History waiting for DB services

--- backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/restapi/DoctorAppointmentsHistory.java
+++ backend/spring-backend-hms/src/main/java/com/hms/springbackendhms/restapi/DoctorAppointmentsHistory.java
@@ -1,8 +1,13 @@
 package com.hms.springbackendhms.restapi;
 
+import com.hms.springbackendhms.config.JwtService;
+import com.hms.springbackendhms.db.VirtualDatabase;
 import com.hms.springbackendhms.response.DoctorAppointmentsHistoryResponse;
 import com.hms.springbackendhms.util.DoctorAppointment;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,24 +20,48 @@ import java.util.List;
 @RequestMapping("/restapi/doctor_appointments_history")
 @RequiredArgsConstructor
 public class DoctorAppointmentsHistory {
-
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @GetMapping
-    public DoctorAppointmentsHistoryResponse history() {
-        ArrayList<DoctorAppointment> appointments = new ArrayList<>();
-        appointments.add(DoctorAppointment.builder()
-                        .patientLastname("Lagomatis")
-                        .patientFirstname("Ilias")
-                        .date(new Date())
-                        .build());
+    public DoctorAppointmentsHistoryResponse history(
+            @CookieValue(name = "token", defaultValue = "") String token
+    ) {
+        if(token.isBlank()){
+            return null;
+        }
 
-        appointments.add(DoctorAppointment.builder()
-                .patientLastname("Mallikopoulou")
-                .patientFirstname("Anastasia")
-                .date(new Date())
-                .build());
+        String userEmail = jwtService.extractUsername(token);
+        if(userEmail != null) {
 
-        return DoctorAppointmentsHistoryResponse.builder()
-                .history(appointments)
-                .build();
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+            if (jwtService.isTokenValid(token, userDetails)) {
+
+                if (VirtualDatabase.hasDoctor(userEmail)) {
+                    // return the history of doctor where mail = userEmail
+
+                    ArrayList<DoctorAppointment> appointments = new ArrayList<>();
+                    appointments.add(DoctorAppointment.builder()
+                            .patientLastname("Lagomatis")
+                            .patientFirstname("Ilias")
+                            .date(new Date())
+                            .build());
+
+                    appointments.add(DoctorAppointment.builder()
+                            .patientLastname("Mallikopoulou")
+                            .patientFirstname("Anastasia")
+                            .date(new Date())
+                            .build());
+
+                    return DoctorAppointmentsHistoryResponse.builder()
+                            .history(appointments)
+                            .build();
+                }
+
+                return null;
+            }
+        }
+
+        return null;
     }
 }
