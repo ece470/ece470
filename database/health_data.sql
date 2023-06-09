a0742edce7461642abd7eaddae7e93467c7da405
markitotrf9 <80339310+markitotrf9@users.noreply.github.com>
2023-06-09 22:27:06 +0300

    Update health_data.sql
    Added the initial database, some modifications are pending

--- database/health_data.sql
+++ database/health_data.sql
@@ -1,4 +1,101 @@
---insert code here
+CREATE DATABASE MedicalService;
+
+DROP TABLE if EXISTS Patient_Info;
+
+CREATE TABLE Patients_Info (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   SecurityNumber int NOT NULL PRIMARY KEY,
+   AFMNumber int NOT NULL, 
+   LastName varchar(255) NOT NULL,
+   FirstName varchar(255) NOT NULL,
+   Email varchar(255) NOT NULL,
+   Dob date NOT NULL,
+   Address varchar(255) NOT NULL,
+   PostalCode varchar(5) NOT NULL, 
+   City varchar(255) NOT NULL,
+   DateOfBirth date NOT NULL,
+   PhoneNumber int NOT NULL 
+   
+);
+
+CREATE TABLE PatientLogInInfo (
+    PatientPassword varchar(32),
+    FOREIGN KEY(PatientID) REFERENCES Patients_info(SecurityNumber)
+
+);
+
+CREATE TABLE History (
+    Injures varchar(1023),
+    Alergies varchar(1023),
+    MedicalExams varchar(1023),
+    Diseases varchar(1023),
+    Vaccines varchar(1023),
+    FOREIGN KEY(PatientID) REFERENCES Patients_info(SecurityNumber)
+);
+
+CREATE TABLE DrugPrescription (
+    DrugName varchar(1023) NOT NULL,
+    DrugInstructions varchar(1023) NOT NULL,
+    FOREIGN KEY(PatientID) REFERENCES Patients_info(SecurityNumber)
+);
+
+CREATE TABLE Exams (
+    ExamDate date,
+    ExamType varchar(1023),
+    ExamInstructions varchar(1023),
+    FOREIGN KEY(PatientID) REFERENCES Patients_info(SecurityNumber)
+);
+
+CREATE TABLE Surgery (
+    SurgeryDate date,
+    SurgeryType varchar(1023),
+    Instructions varchar(1023),
+    Result varchar(1023),
+    FOREIGN KEY(PatientID) REFERENCES Patients_info(SecurityNumber),
+    FOREIGN KEY(DoctorID) REFERENCES Doctors(SecurityNumber)
+);
+
+CREATE TABLE Doctors (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT, #CHECK AUTOINCREMENT
+   SecurityNumber int NOT NULL,
+   AFMNumber int NOT NULL,
+   LastName varchar(255) NOT NULL,
+   FirstName varchar(255) NOT NULL,
+   Email varchar(255) NOT NULL,
+   DobOffice date NOT NULL,
+   CenterAddress varchar(255) NOT NULL,
+   PostalCode varchar(5),
+   City varchar(255) NOT NULL,
+   DateOfBirth date NOT NULL,
+   PhoneNumber int NOT NULL,
+   DoctorType varchar(255) NOT NULL,
+   UserType varchar(20) NOT NULL, 
+   FOREIGN KEY(UserType) REFERENCES UserType(UserType)
+   
+);
+
+CREATE TABLE DoctorLogInInfo (
+    Username varchar(32) UNIQUE,
+    DoctorPassword varchar(32),
+    FOREIGN KEY(DoctorID) REFERENCES Doctors(SecurityNumber)
+);
+
+CREATE TABLE UserType (
+    UserType varchar(50),
+    CONSTRAINT invalidUserType CHECK (UserType IN ('Doctor','Patient','Admin'))
+);
+
+CREATE TABLE Appointment (
+    AppointmentDate date,
+    StartTime hour,  #check what type this is
+    EndTime hour, #check what type this is
+    #Location varchar(255) NOT NULL,
+    Patient varchar(100),
+    Doctor varchar(100),
+    Title varchar(255) NOT NULL,
+    FOREIGN KEY(PatientID) REFERENCES Patients_info(SecurityNumber),
+    FOREIGN KEY(DoctorID) REFERENCES Doctors(SecurityNumber)
+);
 
 CREATE TABLE Appointments (
     
